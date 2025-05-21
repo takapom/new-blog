7cd3deab9063dac078aee44a2b6eb6c2143f65f4
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-05-21 13:07:49 +0900

    UIと使用技術ページ作成

--- blog-backend/posts/views.py
+++ blog-backend/posts/views.py
@@ -6,14 +6,17 @@ from .models import Post
 from .serializers import PostSerializer
 from rest_framework import viewsets
 
+#投稿の一覧表示と新規作成
 class PostListCreateAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.order_by('-created_at')
     serializer_class = PostSerializer
 
+#個別の投稿の詳細表示
 class PostRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+#CRUD処理
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
