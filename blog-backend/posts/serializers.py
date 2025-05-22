a2e10753bbec51c4a3bb77282c5c02fe9e9df68a
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-05-22 13:58:23 +0900

    フォントサイズと記事内容を表示

--- blog-backend/posts/serializers.py
+++ blog-backend/posts/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'created_at', 'develop_lang']
+        fields = ['id', 'title', 'content', 'created_at', 'body', 'develop_lang']
